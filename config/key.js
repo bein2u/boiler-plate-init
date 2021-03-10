5d8eb5d6c2d4f3a7122483a82805bd90b545de5f
jandt <nalary@gmail.com>
2021-03-10 16:11:15 +0900

    github에 push 안되는 에러 처리를 위해 key.js 수정 후 다시 commit

--- config/key.js
+++ config/key.js
@@ -3,3 +3,7 @@ if (process.env.NODE_ENV === 'production') {
 } else {
     module.exports = require('./dev');
 }
+
+// error: src refspec master does not match any
+// error: failed to push some refs to 'https://github.com/bein2u/boiler-plate-init.git'
+// github에 push 안되는 에러 처리를 위해 다시 commit 필요함 (임시로 수정)
